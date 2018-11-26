242eeb04c10083b47f22e211d56589c86a01a9ca
shankar <shankar.edgi01@gmail.com>
2018-11-26 21:36:30 +0530

    CoreJava programs

---
+++ src/main/java/com/shs/basics/SampleJava.java
@@ -0,0 +1,14 @@
+package com.shs.basics;
+
+public class SampleJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		if(i!=0 && i!=1) {
+			System.out.println("value of i"+i);
+		}
+	
+	}
+
+}
